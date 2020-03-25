fd2db6e0810b71e06414352435e03fcec3fb0065
arfurlaneto <arfurlaneto@gmail.com>
2020-03-25 19:09:22 -0300

    Front-end layout finished.

--- web/src/pages/Register/index.js
+++ web/src/pages/Register/index.js
@@ -13,6 +13,7 @@ export default function Register() {
         <section>
           <img src={logoImg} alt="Be The Hero" />
 
+          <h1>Cadastro</h1>
           <p>
             Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.
           </p>
